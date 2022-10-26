95bfd02d9f1da31933eb0e69974c017058c8e248
jkyoo-pc\jkyoo <jk.yoo@solarwiz.co.kr>
2022-10-26 15:28:23 +0900

    .net core console setting

---
+++ wizone_grow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wizone_grow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
